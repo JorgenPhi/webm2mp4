031ce2ade8d8d4a2f2ed4b53c4094f6f66f4444d
Vladislav Kamenev <mazahakaforever@ya.ru>
2018-07-29 22:24:39 +0300

    Thumbnail, progress, improvements

--- bot.js
+++ bot.js
@@ -9,11 +9,64 @@ const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 
 // Check if we have a valid API key
-if (!process.env.TELEGRAMKEY || !process.env.MAXSIZEBYTES || isNaN(process.env.MAXSIZEBYTES)) {
+if (!process.env.TELEGRAMKEY) {
 	console.error('Telegram API key was not, MaxSize was not set, or .env file is missing');
 	process.exit(1);
 }
 
+function processVideo(filename, msg) {
+    ffmpeg(`./tmp/${filename}`)
+        .output(`./tmp/${filename}.mp4`)
+        .outputOptions('-strict -2') // Needed since axc is "experimental"
+        .on('end', () => {
+            var videoStat = fs.statSync(`./tmp/${filename}.mp4`);
+            let fileSizeInBytes = videoStat.size;
+            let fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
+            if (fileSizeInMegabytes >= 10) {
+                console.log('[webm2mp4] File', filename, 'converted - Generating thumbnail...');
+                telegram.sendMessage(msg.chat.id, `Generating thumbnail for: ${filename}...`);
+                ffmpeg(`./tmp/${filename}.mp4`).screenshots({
+                    timestamps: ['50%'],
+                    filename: filename + '.png',
+                    folder: './tmp/'
+                }).on('end', function () {
+                    console.log('[webm2mp4] File', filename, 'finished - Uploading...');
+                    telegram.sendPhoto(msg.chat.id, `./tmp/${filename}.png`).then(function (fileResult) {
+                        fs.unlink('./tmp/' + filename + '.png', () => {});
+                    });
+                });
+            } else {
+                console.log('[webm2mp4] File', filename, 'finished - Uploading...');
+                fs.unlink('./tmp/' + filename, () => {});
+            }
+
+            telegram.sendVideo(msg.chat.id, './tmp/' + filename + '.mp4').then(function() {
+                fs.unlink('./tmp/' + filename + '.mp4', () => {});
+            });
+        })
+        .on('progress', function(progress) {
+            telegram.sendMessage(msg.chat.id, 'Processing: ' + progress.percent + '% done', {disable_notification: true}).then((result) => {
+                setTimeout(function () {
+                    telegram.deleteMessage(msg.chat.id, result.message_id);
+                }, 500);
+            });
+        })
+        .on('error', (e) => {
+            console.error(e);
+            // Cleanup
+            fs.unlink('./tmp/' + filename, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+            fs.unlink('./tmp/' + filename + '.mp4', (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        })
+        .run();
+}
 
 function initListeners(username) {
 	// Telegram Required Commands
@@ -24,123 +77,76 @@ function initListeners(username) {
 	username = username.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 
 	// These commands are required to have responses by the Telegram API 
-	telegram.onText(RegExp('/start(?:@' + username + ')?$', 'i'), function(msg, match) {
+	telegram.onText(new RegExp('/start(?:@' + username + ')?$', 'i'), function(msg, match) {
 		console.log('[webm2mp4] New private chat started with', msg.from);
 		telegram.sendMessage(msg.chat.id, 'Hello! Upload an WebM for me to convert it to a MP4.');
 	});
 
-	telegram.onText(RegExp('/help(?:@' + username + ')?$', 'i'), function(msg, match) {
+	telegram.onText(new RegExp('/help(?:@' + username + ')?$', 'i'), function(msg, match) {
 		console.log('[webm2mp4] Help command used by', msg.from);
 		telegram.sendMessage(msg.chat.id, 'Hello! Upload an WebM for me to convert it to a MP4. I can also be added to group chats to automatically convert WebMs.');
 	});
 
-	telegram.onText(new RegExp('(https?:\\/\\/[^\\s]+.webm)'), function (msg, match) {
+	telegram.onText(/^(http|https).*/, function (msg, match) {
 		let filename;
-        let r = request(match[0]).on('response', function (res) {
-            let contentDisp = res.headers['content-disposition'];
-            if (contentDisp && /^attachment/i.test(contentDisp)) {
-                filename = contentDisp.toLowerCase()
-                    .split('filename=')[1]
-                    .split(';')[0]
-                    .replace(/"/g, '');
-            } else {
-                filename = path.basename(url.parse(match[0]).path);
+		let status;
+        let r = request(match[0]);
+        telegram.sendMessage(msg.chat.id, `Started downloading`).then((result) => {
+            setTimeout(() => {
+                telegram.deleteMessage(msg.chat.id, result.message_id);
+            }, 5000);
+        });
+
+        r.on('response', function (res) {
+        	if (res.statusCode !== 200) {
+        		status = false;
+                telegram.sendMessage(msg.chat.id, `Failed to download. Code: ${res.statusCode}`);
+                return;
+			}
+			if (res.headers['content-type'].indexOf('video') === -1) {
+        	    status = false;
+                telegram.sendMessage(msg.chat.id, `Doesn't look like a video`);
+                return;
             }
+
+            filename = path.basename(url.parse(match[0]).path);
             console.log(filename);
-            r.pipe(fs.createWriteStream(path.join(__dirname, filename)));
+            status = true;
+            r.pipe(fs.createWriteStream(path.join(__dirname, '/tmp/', filename)));
         });
 
         r.on('end', function () {
-            ffmpeg(filename)
-                .output(filename + '.mp4')
-                .outputOptions('-strict -2') // Needed since axc is "experimental"
-                .on('end', () => {
-                    // Cleanup
-                    fs.unlink(filename, (e) => {
-                        if (e) {
-                            console.error(e);
-                        }
-                    });
-                    console.log('[webm2mp4] File', filename, 'converted - Uploading...');
-                    telegram.sendVideo(msg.chat.id, filename + '.mp4').then(function() {
-                        fs.unlink(filename + '.mp4', (e) => {
-                            if (e) {
-                                console.error(e);
-                            }
-                        });
-                    });
-                })
-                .on('error', (e) => {
-                    console.error(e);
-                    // Cleanup
-                    fs.unlink(filename, (err) => {
-                        if (err) {
-                            console.error(err);
-                        }
-                    });
-                    fs.unlink(filename + '.mp4', (err) => {
-                        if (err) {
-                            console.error(err);
-                        }
-                    });
-                })
-                .run();
+        	if (!status) {return;}
+			processVideo(filename, msg)
 		});
+
+        r.on('error', function (err) {
+        	status = false;
+        	telegram.sendMessage(msg.chat.id, `Failed to download video. Reason: ${err}`);
+            telegram.sendMessage(msg.chat.id, `Debug info: link: ${match[0]}\nfilename: ${filename}`);
+		})
 	});
 
 	// The real meat of the bot
 	telegram.on('document', (msg) => {
 		if (msg.document.mime_type === 'video/webm') {
-			// Check the file size
-			if (msg.document.file_size > process.env.MAXSIZEBYTES) {
-				console.log(process.env.MAXSIZEBYTES)
-				console.log('[webm2mp4] ', msg.from, ' uploaded a file that was too big.');
-				telegram.sendMessage(msg.chat.id, 'This file is too large for me to convert. It must be less than ' + prettysize(process.env.MAXSIZEBYTES) + '.');
-				return;
-			}
 			// Download it
-			telegram.downloadFile(msg.document.file_id, './tmp/').then(function(filename) {
-				ffmpeg(filename)
-					.output(filename + '.mp4')
-					.outputOptions('-strict -2') // Needed since axc is "experimental"
-					.on('end', () => {
-						// Cleanup
-						fs.unlink(filename, (e) => {
-							if (e) {
-								console.error(e);
-							}
-						});
-						console.log('[webm2mp4] File', msg.document.file_name, 'converted - Uploading...');
-						telegram.sendVideo(msg.chat.id, filename + '.mp4').then(function() {
-							fs.unlink(filename + '.mp4', (e) => {
-								if (e) {
-									console.error(e);
-								}
-							});
-						});
-					})
-					.on('error', (e) => {
-						console.error(e);
-						// Cleanup
-						fs.unlink(filename, (err) => {
-							if (err) {
-								console.error(err);
-							}
-						});
-						fs.unlink(filename + '.mp4', (err) => {
-							if (err) {
-								console.error(err);
-							}
-						});
-					})
-					.run();
-			})
+
+            telegram.sendMessage(msg.chat.id, `Started downloading`).then((result) => {
+                setTimeout(() => {
+                    telegram.deleteMessage(msg.chat.id, result.message_id);
+                }, 5000);
+            });
+
+            telegram.downloadFile(msg.document.file_id, './tmp/').then(function(filename) {
+			    filename = filename.split(path.sep)[1];
+				processVideo(filename,msg);
+			}).catch((e) => telegram.sendMessage(msg.chat.id, `Failed to download video. Reason: ${e}`));
 		}
 	});
 }
 
 // Init
-process.env.MAXSIZEBYTES = parseInt(process.env.MAXSIZEBYTES);
 if (!fs.existsSync('./tmp/')) {
 	fs.mkdirSync('./tmp/');
 }
